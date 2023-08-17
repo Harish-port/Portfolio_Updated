29e8cbc568e956ce452971573d2090a5af6893e2
Harish S <46044707+Harish-port@users.noreply.github.com>
2023-08-17 15:36:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from './Layouts/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Layouts/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contact</p>
+    </div>
+  )
+}
+
+export default Header;
